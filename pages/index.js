28d088de26a22b3b03df605f9eba0847770bac90
emelies <emelie.sabel@hyperisland.se>
2021-04-03 18:21:44 +0200

    title

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>emeliesabel</title>
         <link rel="icon" href="/favicon.svg" />
       </Head>
       <HeroHeader />
